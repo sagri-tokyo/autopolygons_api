f4b049a665369304e99e95d31716da9dce54baa1
moonorange <monoma632@gmail.com>
2021-02-08 16:53:54 +0900

    fix: typo

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.gis.db import models
+from prefectures.models import Prefecture
 
-# Create your models here.
+class City(models.Model):
+	prefecture = models.ForeignKey(Prefecture, on_delete=models.CASCADE)
+	name = models.CharField(blank=False, null=False, max_length=255)
+	geom = models.MultiPolygonField(blank=False, null=True)
+
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(
+				name="unique_city",
+				fields=["prefecture", "name"]
+			)
+		]
+		db_table = "cities"
+
+	def __str__(self):
+		return self.name
--- prefectures/models.py
+++ prefectures/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Prefecture(models.Model):
+	name = models.CharField(blank=False, null=False, unique=True, max_length=255)
+
+	class Meta:
+		db_table = "prefectures"
+
+	def __str__(self):
+		return self.name
