4b44b156b8608ec6a28fc332015f02597198505d
moonorange <monoma632@gmail.com>
2021-02-08 18:43:24 +0900

    add: insert pref script

---
+++ scripts/seeds.py
@@ -0,0 +1,16 @@
+import os
+from django.contrib.gis.geos import Polygon, MultiPolygon
+from farmlands.models import Farmland
+from prefectures.models import Prefecture
+from cities.models import City
+import glob
+import re
+
+PREFECTURES = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
+
+BATCH_SIZE = 1000
+
+def create_prefectures():
+	for pref in PREFECTURES:
+		pref_obj = Prefecture(name=pref)
+		pref_obj.save()
