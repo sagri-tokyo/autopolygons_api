4cf020450cb00db2031a1ba4d274450918f1cb1c
moonorange <monoma632@gmail.com>
2021-02-12 10:55:26 +0900

    add: calculate intersection and union

--- scripts/seeds.py
+++ scripts/seeds.py
@@ -10,7 +10,7 @@ import re
 import pdb
 from .layer_mappings.custom_polygon_layer_mapping import CustomPolygonLayerMapping
 from django.db.models import F
-from django.contrib.gis.db.models.functions import Intersection
+from django.contrib.gis.db.models.functions import Intersection, Union
 
 PREFECTURES = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県']
 
@@ -74,7 +74,8 @@ def add_city_relation_to_farmlands():
 
 def union_overlapped_farmlands():
 	for polygon in chunkator(Farmland.objects.all(), BATCH_SIZE):
-		overlapped_polygons = Farmland.objects.filter(geom__intersects=polygon.geom).all().annotate(intersection=Intersection(F('geom'), polygon.geom))
+		overlapped_polygons = Farmland.objects.filter(geom__intersects=polygon.geom).all(
+		).annotate(intersection=Intersection(F('geom'), polygon.geom), union=Union(F('geom'), polygon.geom))
 
 
 def run():
