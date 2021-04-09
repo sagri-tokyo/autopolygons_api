f543bf05f11fb583b987c6bccd706cd9b72434c0
moonorange <monoma632@gmail.com>
2021-04-09 11:03:01 +0900

    fix: redundant commands and file

--- docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-set -e
-psql postgres << EOSQL
-  CREATE DATABASE autopolygon_api WITH OWNER = postgres TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'ja_JP.UTF-8' LC_CTYPE = 'ja_JP.UTF-8';
-EOSQL
