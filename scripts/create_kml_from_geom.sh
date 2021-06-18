4f4642c0270f86d35fe8d0c4d0a9c2755af2461e
moonorange <monoma632@gmail.com>
2021-06-18 17:12:19 +0900

    fix: slash

--- scripts/create_kml_from_geom.sh
+++ scripts/create_kml_from_geom.sh
@@ -3,4 +3,4 @@
 # postgresqlのデータをshpに変換
 pgsql2shp -f shp/output.shp -u postgres -h unionpolygon_postgres $1 farmlands
 # shpをkmlに変換
-ogr2ogr -f KML kml/output.kml shp//output.shp
+ogr2ogr -f KML kml/output.kml shp/output.shp
